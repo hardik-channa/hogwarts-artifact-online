01b253660929f3eef787970fd66c030236c8a252
hardik-channa <hardikchanna@gmail.com>
2025-05-23 11:27:16 +0530

    This is first commit

---
+++ src/main/java/com/hardik/hogwards_artifact_online/HogwardsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.hogwards_artifact_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardsArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwardsArtifactOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/hogwards_artifact_online/HogwardsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.hogwards_artifact_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwardsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
